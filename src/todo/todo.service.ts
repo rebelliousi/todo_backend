0ff8669bb78ba50f9f9da502174cc4780fdada6b
rebelliousi <learneryolo3@gmail.com>
2026-08-05 10:54:42 +0500

    create todo service and create method

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { Todo } from './entities/todo.entity';
+import { CreateTodoDto } from './dto/create-todo.dto';
 
 @Injectable()
 export class TodoService {
   private todos: Todo[] = [];
   private idCounter = 1;
+
+  create(createTodoDto: CreateTodoDto) {
+    const newTodo: Todo = {
+      id: this.idCounter,
+      title: createTodoDto.title,
+      description: createTodoDto.description,
+      completed: createTodoDto.completed,
+    };
+    this.todos.push(newTodo);
+    this.idCounter++;
+    return newTodo;
+  }
 }
