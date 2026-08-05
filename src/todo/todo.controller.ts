a1aa89ab35cd6fa31163b80268dddc2ed92e9bc3
rebelliousi <learneryolo3@gmail.com>
2026-08-05 12:49:37 +0500

    feat:add new update method to controller

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
 
 @Controller('todo')
 export class TodoController {
@@ -18,5 +19,9 @@ export class TodoController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.todoService.findOne(+id);
+  }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatTodDto: UpdateTodoDto) {
+    return this.todoService.update(+id, updatTodDto);
   }
 }
