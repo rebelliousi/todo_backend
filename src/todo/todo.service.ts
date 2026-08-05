2b88d2ae0a290747a7e9aca11718191bb92d1b5f
rebelliousi <learneryolo3@gmail.com>
2026-08-05 11:45:42 +0500

    feat:add notfoundexcetion for  if id is not found

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Todo } from './entities/todo.entity';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
@@ -7,6 +7,13 @@ import { UpdateTodoDto } from './dto/update-todo.dto';
 export class TodoService {
   private todos: Todo[] = [];
   private idCounter = 1;
+  private findTodoOrThrow(id: number): Todo {
+    const todo = this.todos.find((todo) => todo.id === id);
+    if (!todo) {
+      throw new NotFoundException(`Todo #${id} not found`);
+    }
+    return todo;
+  }
 
   create(createTodoDto: CreateTodoDto) {
     const newTodo: Todo = {
@@ -25,13 +32,15 @@ export class TodoService {
   }
 
   findOne(id: number) {
-    return this.todos.find((todo) => todo.id === id);
+    return this.findTodoOrThrow(id);
   }
   update(id: number, updateTodoDto: UpdateTodoDto) {
+    this.findTodoOrThrow(id);
     const index = this.todos.findIndex((todo) => todo.id === id);
     return (this.todos[index] = { ...this.todos[index], ...updateTodoDto });
   }
   remove(id: number) {
+    this.findTodoOrThrow(id);
     const newTodo = this.todos.filter((todo) => todo.id !== id);
     return (this.todos = newTodo);
   }
